6b554757879cf7cc7a30d0920c7ef50a6f0e9593
ashwin alexander <alexwin75@gmail.com>
2023-08-21 22:43:52 -0400

    generate pwd and save to clipboard

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from tkinter import *
 from tkinter import messagebox
-
-
-
+from random  import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_pwd():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    password_lett = [choice(letters) for _ in range(randint(8, 10))]
+    password_sym = [choice(symbols) for _ in range(randint(2, 4))]
+    password_num = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_lett + password_sym + password_num
+    shuffle(password_list)
+
+    password = ''.join(password_list)
+    pyperclip.copy(password)
+    txtPassword.delete(0,END)
+    txtPassword.insert(0, password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -77,8 +91,9 @@ var_password = StringVar()
 txtPassword = Entry(width=33, textvariable=var_password)
 txtPassword.grid(row=3,column=1)
 
+
 # Generate Password button
-btnGen = Button(text="Generate Password", command=save_entry)
+btnGen = Button(text="Generate Password", command=gen_pwd)
 btnGen.grid(row=3, column=2)
 
 # Add button
